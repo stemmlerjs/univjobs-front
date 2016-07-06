f7102b3a1e0f52169048f5e93014bbe4ed7e6af1
stemmlerjs <kstemmler@bcsglobal.ca>
2016-07-06 13:01:45 -0400

    Discovered the Beauty of childContextTypes This is used with the <Provider> that we nest our routes in. Its good because this means that if we want to access the store from one of our components, we can do so through grabbing the store off of our context in any Stateful component.

--- app/containers/Sidebar/SidebarContainer.js
+++ app/containers/Sidebar/SidebarContainer.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Sidebar } from 'components/'
 const SidebarContainer = React.createClass({
+  // <Provider> implicitly does .childContextTypes and passes down the store object to all components.
+  // We can grab this by specifying the context types we want
+  contextTypes: {
+    store: React.PropTypes.object
+  },
   render () {
+    const { store } = this.context;
+    console.log(store);
     return (
       <Sidebar/>
     )
   },
 })
+
+
 
 export default SidebarContainer
