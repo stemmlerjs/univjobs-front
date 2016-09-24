8c2c63cfa6a3e8605c2a5ff8de81a8ddeada149e
Charles Javelona <charlesjavelona@gmail.com>
2016-09-23 23:58:18 -0400

    Card component modularize refinement

--- app/modules/Dashboard/components/StudentDashboard.js
+++ app/modules/Dashboard/components/StudentDashboard.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+//NOTE: export Card.js then import it here
 import SkyLight from 'react-skylight'
 import { pageContainer, cardContainer, card, cardHeader, 
 	jobTitleContainer, jobTitle, industryTitle,
@@ -31,53 +32,6 @@ const StudentDashboard = React.createClass({
   render() {
 	 //Pass user info with job info then loop to show
    return (
-       <div className={pageContainer}>
-         <div className={cardContainer}>
-	    <div className={card}>
-	      <header className={cardHeader}>
-	       <span>
-	         <p>Job Type</p>
-	       </span>
-	      </header>
-	      <div className={jobTitleContainer}>
-	        <h2 className={jobTitle}>Job Title</h2>
-	      </div>
-	      <h3 className={industryTitle}>Software Development</h3>
-
-	      <div>
-	        <ul className={tagContainer}>
-	         <li className={tagElement}>
-		  <span>Test</span>
-		 </li>
-	         <li className={tagElement}>
-		  <span>Test</span>
-		 </li>
-	         <li className={tagElement}>
-		  <span>Test</span>
-		  </li>
-	         <li className={tagElement}>
-		  <span>Test</span>
-		  </li>
-		</ul>
-	      </div>
-
-	      <div>
-	        <ul className={companyContainer}>
-		 <li>
-		   <img src="https://placeholdit.imgix.net/~text?txtsize=12&txt=56%C3%9756&w=56&h=56" alt="Smiley face"/>
-		 </li>
-		 <li>
-		  <ul className={companyInfoContainer}>
-		    <li><h4 className={companyTitle}>Univjobs</h4>
-		    </li>
-		    <li>
-		      <a href="https://google.com">Company Info</a>
-		    </li>
-		  </ul> 
-		 </li>
-		</ul>
-	      </div>
-	      <button className={applyButton} onClick={() => this.refs.jobModal.show()}>APPLY</button>
 	      <SkyLight
 	      	overlayStyles={styles.overlayStyles}
 	        dialogStyles={styles.dialogStyles}
