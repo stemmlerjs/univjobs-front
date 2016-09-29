bf653ec9a6e95119666a1ab0c34de2a9b54ae995
Charles Javelona <charlesjavelona@gmail.com>
2016-09-29 19:33:27 -0400

    student dashboard test

--- app/helpers/studentDashboard.js
+++ app/helpers/studentDashboard.js
@@ -3,15 +3,31 @@
  */
 import axios from 'axios'
 import config from 'config'
-//import { getJobList } from 'redux/modules/dashboard/studentDashboard'
+import { getJobList } from 'redux/modules/dashboard/dashboard'
 
 export function getJobs(store) {
  const promise = new Promise((resolve, reject) => {
-  axios.get(config.baseUrl + 'job_list')
-  .then((response) => {
-    store.dispatch(getJobList('GENERAL_JOBS', response.data))
-	resolve(true);
-  })
+
+   //Check if user is a student && isProfileCompleted
+   if(store.user.isAStudent && store.user.isProfileCompeleted) {
+	   axios.get(config.baseUrl + 'job')
+		   .then((response) => {
+			   //store.dispatch(getJobList('FETCH_JOBS', response.data))
+			   console.log(response.data)
+			   resolve(true);
+		   })
+   } else if(store.user.isAStudent && !store.user.isProfileCompleted) {
+   	//Route back to student profile
+	console.log('isAStudent && profile not completed')
+	console.log('Route back to student profile')
+   } else if(!store.user.isAStudent && store.user.isProfileCompleted) {
+    	//Route back to employer profile
+	console.log('Is not a student && profile completed')
+	console.log('Route back to employer categories')
+   } else {
+   	//Logout user? 
+	console.log('logout user')
+   }
   .catch(()=> {
     resolve(false);
   })
--- app/redux/modules/dashboard/dashboard.js
+++ app/redux/modules/dashboard/dashboard.js
@@ -1,10 +1,75 @@
+import { getJobs } from 'helpers/studentdashboard'
+
 /*==================================================
+ *
  *  STUDENT & EMPLOYER  DASHBOARD INITIAL STATE
  *===============================================
-*/ 
+*/
+const intialState = {
+  jobs: {},
+  error: ''
+}
 
 
+/*===========================================
+ *  		ACTIONS 
+ *==========================================
+*/
 
+//** GETTER actions **//
+const FETCH_JOBS = 'DASHBOARD.FETCH_JOBS'
 
+//** Dasbord List SUCCESS actions **//
+const FETCHED_JOBS = 'DASHBOARD.FETCHED_JOBS'
 
+/*===========================================
+ *  		ACTION CREATORS
+ *==========================================
+*/
 
+//# FIXME: 
+export function getJobList( ,jobList) {
+	return {
+		type: FETCH_JOBS,
+		listType: FETCHED_JOBS,
+		list: jobList
+	}
+}
+
+/*===========================================
+ *  		REDUCERS
+=============================================
+*/
+
+const initialJobListState = {
+	id: '',
+	type: '',
+	title: '',
+	paid: '',
+	start_date: '',
+	responsibilities: '',
+	qualification: '',
+	address: '',
+	city: '',
+	compensation: '',
+	//NOTE: q1 & q2 will be deleted due to new tables added
+	question_1: '',
+	question_2: '',
+	max_applicants: '',
+	active: '',
+	verified: '',
+	propsErrorMap: {}
+}
+
+export default function jobs(state= initialJobListState, action) {
+	switch(action.type) {
+	    case FETCH_JOBS:
+	      return {
+		...state,
+		jobs: getJobs(action) 
+	      }		
+
+	    default:	
+	      return state
+	}//switch
+}
