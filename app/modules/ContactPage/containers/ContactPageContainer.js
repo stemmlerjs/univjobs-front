985556b9712d47b015877b6f3460e615240556d4
Charles Javelona <charlesjavelona@gmail.com>
2017-01-10 10:18:03 -0500

    Added some validation and some React toastr to inputs.

--- app/modules/ContactPage/containers/ContactPageContainer.js
+++ app/modules/ContactPage/containers/ContactPageContainer.js
@@ -1,11 +1,24 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
+// =============REDUX STATE & IMPORTS========================== //
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+// ============================================================ //
+
 // ============= OTHER IMPORTS========================== //
 import { ContactPage } from 'modules/ContactPage'
 import { Footer, RegularNav } from 'modules/Main'
 import { contactPage } from '../styles/ContactPage.css'
 import { sendMessage } from 'helpers/contact'
+// ==================MESSAGES============================== //
+import ReduxToastr from 'react-redux-toastr'
+import {toastr} from 'react-redux-toastr'
+
+// ====================================== //
+
+const actionCreators = {
+}
 
 /*NOTE: Using ES6 class, please see:
  *  https://toddmotto.com/react-create-class-versus-component/
@@ -46,14 +59,40 @@ class ContactPageContainer extends React.Component {
     }
     handleSendMessage(e) {
         e.preventDefault()
-
-        const data = {
-            senderType: this.state.senderType,
-            senderName: this.state.senderName,
-            senderEmail: this.state.senderEmail,
-            senderMessage: this.state.senderMessage
+        
+        //TODO: Refactor into a util function. 
+        //          Append into an object or array 
+        //          where it checks each key's value empty or not
+        //          return true or false
+        //
+        //      Add a checker for all inputs
+        //          Must have proper email
+        //          Must sanitize all inputs to prevent any bad injections. 
+        //
+        //      NOTE: Should we even sanitize this? 
+        //
+        if(this.state.senderName.length > 0 && this.state.senderEmail.length > 0 && this.state.senderMessage.length > 0) {
+            const data = {
+                senderType: this.state.senderType,
+                senderName: this.state.senderName,
+                senderEmail: this.state.senderEmail,
+                senderMessage: this.state.senderMessage
+            }
+            sendMessage(data)
+            .then((response) => {
+                if(response.success) {
+                    toastr.success(response.message)
+                } else {
+                    toastr.error(response.error)
+                }
+            })
+            .catch((err) => {
+                toastr.error(err)
+            })
+        } else {
+            toastr.error('Please fill out everything before sending')
         }
-        sendMessage(data)
+
     }
     componentWillMount() {
         // Hide the overlay on mount if coming from direct URL
@@ -75,6 +114,11 @@ class ContactPageContainer extends React.Component {
                     onHandleSendMessage={(e) => this.handleSendMessage(e)}      
                 /> 
                <Footer />
+               <ReduxToastr
+                 timeOut={4000}
+                 newestOnTop={false}
+                 position="top-right"
+                />
              </div>
         )
     }
@@ -83,5 +127,30 @@ class ContactPageContainer extends React.Component {
 ContactPageContainer.propTypes = {
     router: React.PropTypes.string
 }
+/* The entire redux store is passed in here,
+// Return an object defining which values you want to bind to props
+//
+// @params ({user}) contains BaseUser & Employer attributes
+// */
         
-export default ContactPageContainer
+function mapStateToProps({}) {
+  return {
+  }
+}
+
+/**
+  * mapActionCreatorsToProps
+  *
+  * This function grabs all of the Action Creators on the object of the first parameter in the bindActionCreators function
+  * and makes them available to us through THIS component's props (SignupContainer, this.props). We can then pass these to our child
+  * components to use. DON'T MAKE A HABIT of doing this too deeply (drilling prop holes).
+  *
+  **/
+
+function mapActionCreatorsToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch)
+}
+
+// connect(specify_what_keys_you_want_from_store, wraps_dispatch_around_action_creators)(container)
+//
+export default connect(mapStateToProps, mapActionCreatorsToProps)(ContactPageContainer)
