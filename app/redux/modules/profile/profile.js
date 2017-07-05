4ada261aeb8e50cd189960b4a1b846453b88f3ff
OldAndInYourWay <metroidman12@gmail.com>
2017-07-05 18:45:23 -0400

    Fixed GPA problem. GPA wasn't being saved on initial profile creation. had to change from GPA to gpa in api call.

--- app/redux/modules/profile/profile.js
+++ app/redux/modules/profile/profile.js
@@ -222,7 +222,7 @@ export function submitProfileFirstTime(userTypeInt, profileInfo, user, successCa
               enroll_date: toISO(profileInfo.enrollmentDate),
               grad_date: toISO(profileInfo.graduationDate),
               major_id: profileInfo.major.id ? profileInfo.major.id : profileInfo.major,
-              GPA: JSON.stringify(parseFloat(profileInfo.gpa)),
+              gpa: JSON.stringify(parseFloat(profileInfo.gpa)),
               personal_email: profileInfo.personalEmail,
               gender: profileInfo.gender.id ? profileInfo.gender.id : profileInfo.gender,
                 /*Converts the value to num*/
