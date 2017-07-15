8b787a20492302d78322d0f9d1bac16da457e51e
Khalil Stemmler <metroidman12@gmail.com>
2017-07-15 18:27:31 -0400

    Add dev loggin

--- server.js
+++ server.js
@@ -3,15 +3,15 @@
 
 var express = require('express')
 var path = require('path')
-// var morgan = require('morgan')
+var morgan = require('morgan')
 
 app = express();
 app.listen(80, function() {
   console.log("[Node] Front-end server running on port 80")
 })
 
-app.use(express.static('dist'))  
-// app.use(morgan('dev'));  
+app.use(express.static('dist'))
+app.use(morgan('dev'));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
