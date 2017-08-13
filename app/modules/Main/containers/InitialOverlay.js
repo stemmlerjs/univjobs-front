9eb2e519895b7ae087e3635676943191695777e5
Khalil Stemmler <metroidman12@gmail.com>
2017-08-12 23:47:12 -0400

    Needed to add a final forward slash to get the redirect to work

--- app/modules/Main/containers/InitialOverlay.js
+++ app/modules/Main/containers/InitialOverlay.js
@@ -84,7 +84,7 @@ const InitialOverlay = React.createClass({
     *    to the appropriate page.
     */
 
-    if (window.location.href === "https://univjobs.ca") {
+    if (window.location.href === "https://univjobs.ca/") {
       console.log('[Univjobs]: At main page. Redirecting to /join.')
       window.location.assign('/join')
     }
