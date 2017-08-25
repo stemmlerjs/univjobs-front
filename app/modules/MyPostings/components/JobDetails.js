760b54cd37d5c6df967e4cb6c80139f9c3291e3c
Khalil Stemmler <metroidman12@gmail.com>
2017-08-24 22:26:37 -0400

    save

--- app/modules/MyPostings/components/JobDetails.js
+++ app/modules/MyPostings/components/JobDetails.js
@@ -1,11 +1,37 @@
 
 import React, { PropTypes } from 'react'
 
+import { Combobox, DropdownList, DateTimePicker, Calendar, Multiselect, SelectList} from 'react-widgets'
 import { ApplicantCount, StartDateComponent, PaidJobComponent, LocationComponent }  from 'modules/SharedComponents'
 
 import { textDetailsTitle, textDetailsField, title, jobTypeText } from '../styles/JobDetailsStyles.css'
 import { box, editableArea, editableAreaResizeable, editableTitleArea, standardButton, 
     standardButtonRed, standardButtonInactive, saveButtonsContainer } from '../styles/MyPostingsStyles.css'
+
+
+const jobTypeList = [
+{
+  type: 1,
+  text: 'One Time Gig'
+},{
+  type: 2,
+  text: 'Summer'
+},{
+  type: 3,
+  text: 'Winter'
+},{
+  type: 4,
+  text: 'Freelance'
+},{
+  type: 5,
+  text: 'Campus Rep'
+},{
+  type: 6,
+  text: 'Part-time'
+},{
+  type: 7,
+  text: 'Full-time'
+},]
 
 export default function JobDetails ({ 
   jobTitle, 
@@ -35,25 +61,37 @@ export default function JobDetails ({
           : <textarea onBlur={(e) => handleUpdateJobDetailsField(e.target.value, 'title', page)} 
             defaultValue={jobTitle} className={editableTitleArea}></textarea>
       }
+
+      {
+        !editViewEnabled
+          ?  <div className={jobTypeText}>{
+                  jobType === 1 
+                  ? 'One Time Gig' :
+                  jobType === 2 
+                  ? 'Summer' :
+                  jobType === 3 
+                  ? 'Winter' :
+                  jobType === 4
+                  ? 'Freelance' :
+                  jobType === 5
+                  ? 'Campus Rep' :
+                  jobType === 6
+                  ? 'Part-time' :
+                  jobType === 7 
+                  ? 'Full-time' :
+                  ''
+                }
+              </div>
+          : <DropdownList
+              
+              valueField="type" textField="text"
+              data={jobTypeList}
+              value={jobType}
+              defaultValue={jobType}
+              onChange={value => handleUpdateJobDetailsField(value.type, 'responsibilities', page)}
+            />
+      }
       
-      <div className={jobTypeText}>{
-          jobType === 1 
-          ? 'One Time Gig' :
-          jobType === 2 
-          ? 'Summer' :
-          jobType === 3 
-          ? 'Winter' :
-          jobType === 4
-          ? 'Freelance' :
-          jobType === 5
-          ? 'Campus Rep' :
-          jobType === 6
-          ? 'Part-time' :
-          jobType === 7 
-          ? 'Full-time' :
-          ''
-        }
-      </div>
       
       <StartDateComponent date={new Date(startDate)}/>
       <LocationComponent location={location} remoteWork={remoteWork}/>
