d99c8dfc1ae453b453c9f423fadaae906130396e
Khalil Stemmler <metroidman12@gmail.com>
2017-09-07 12:35:52 -0400

    Made changes to about page

--- app/modules/AboutPage/components/AboutPage.js
+++ app/modules/AboutPage/components/AboutPage.js
@@ -11,7 +11,7 @@ import { blackBackGround,flexContainer, flexRowContainer, flexRowItem,
          regularFlexContainer, subHeader, topMarginReduce,
          card, threeColumn, grayBackGround, section, picture,
          personsName, title, paragraph, 
-        charlesImg, khalilImg, georgeImg, julioImg, aboutUsTitle }from '../styles/AboutPage.css'
+        charlesImg, khalilImg, georgeImg, julioImg, aboutUsTitle, cardDetailsContainerClosed, cardDetailsContainerOpen, seeMoreButton }from '../styles/AboutPage.css'
 
 /*
  *  * NOTE: Testing ES6 stateless function
@@ -20,7 +20,7 @@ import { blackBackGround,flexContainer, flexRowContainer, flexRowItem,
  *   NOTE: Maybe use scroll http://stackoverflow.com/questions/30495062/how-can-i-scroll-a-div-to-be-visible-in-reactjs
  */
 
-const AboutPage = () => (
+const AboutPage = ( { toggleCardOpen, cardsState }) => (
         <div>
             <div id="about-us-hero" className={flexContainer}>
               <div className={header}>
@@ -40,9 +40,15 @@ const AboutPage = () => (
                         </div>
                         <h3 className={personsName}>Charles Javelona</h3>
                         <h4 className={title}>CEO, Founder</h4>
-                        <p className={paragraph}>
-                            Charles previously worked at Scotiabank Innovation Factory, delving into Fintech analysis and blockchain development. He is originally from the Philippines.
-                        </p>
+
+                        <div className={!cardsState.charles ? cardDetailsContainerClosed : cardDetailsContainerOpen}>
+                          <p className={paragraph}>
+                              Originally from the Philippines, Charles previously worked at Scotiabank Innovation Factory.  In his role, he specialized in Fintech analysis and blockchain development.  
+                                </p>
+                          <p className={paragraph}>An aspiring entrepreneur, Charles has an impressive track record for innovation and making the impossible happen. From starting a Toastmasters Club in Oakville and having recognized a need for improved communication capabilities among students to braving the world of entrepreneurship, Charles simply views obstacles as challenges to be overcome.  </p>
+                          <p className={paragraph}>Relentless in his pursuit of excellence, Charles is continuously looking for ways to increase Univjob’s value to both Employers & Students.  As the founder of Univjobs, Charles’ unbridled passion is contagious and the driving force behind the company’s success.</p>
+                        </div>
+                        <button className={seeMoreButton} onClick={() => toggleCardOpen('charles')}>{!cardsState.charles ? "See More" : "See Less"}</button>
                     </div>
                     {/*SECOND CARD*/}
                     <div className={card}> 
@@ -51,21 +57,33 @@ const AboutPage = () => (
                         </div>
                         <h3 className={personsName}>Khalil Stemmler</h3>
                         <h4 className={title}>CTO, Founder</h4>
-                        <p className={paragraph}>
-                            Prior to Univjobs, Khalil worked in software development at IBM and Pinnaca in between his schooling at Brock University and Sheridan College.
-                            His favourite things include cats, tinkering with robotics and collecting music; though not necessarily in that order.
-                        </p>
+
+                        <div className={!cardsState.khalil ? cardDetailsContainerClosed : cardDetailsContainerOpen}>
+                          <p className={paragraph}>
+                              Prior to becoming a founding member of the Univjobs team, Khalil worked in software development at IBM and Pinnaca while attending school at both Brock University and Sheridan College.   </p>
+                          <p className={paragraph}>As a Software Architect, Khalil works diligently to ensure that the platform is scalable, secure, and mobile friendly for employers and students.  </p>
+                          <p className={paragraph}>His software knowledge and implementation expertise consistently amazing his teammates and serves to bolster the confidence and commitment of the entire team. Outside of work, Khalil is equally passionate to his hobbies which include: robotics, cats, and collecting music. </p>
+                        </div>
+
+                      <button className={seeMoreButton} onClick={() => toggleCardOpen('khalil')}>{!cardsState.khalil ? "See More" : "See Less"}</button>
                     </div>
                     {/*THIRD CARD*/} 
                     <div className={card}> 
                         <div className={picture}>
                             <img className={georgeImg}/>              
                         </div>
+   
                         <h3 className={personsName}>George Kavuma</h3>
                         <h4 className={title}>Chief Design Officer, Founder</h4>
-                        <p className={paragraph}>
-                            George is an interaction designer who has previously worked with small companies. If he's not preparing for a marathon race, you'll find George working on instrumentals beats or improving his photography skills.
-                        </p>
+
+                        <div className={!cardsState.george ? cardDetailsContainerClosed : cardDetailsContainerOpen}>
+                          <p className={paragraph}>
+                              George is an Interaction Designer who has honed his skills by working with a variety of small companies where his work was deemed essential to their success. </p>
+                          <p className={paragraph}>Driven by his passion for design, George obsesses over ensuring that both students and employers utilizing Univjobs enjoy the best user experience possible.  </p>
+                          <p className={paragraph}>Outside of work, when he is not preparing for a marathon race, you will find George working on instrumental beats or honing his photography skills.  </p>
+                        </div>
+
+                        <button className={seeMoreButton} onClick={() => toggleCardOpen('george')}>{!cardsState.george ? "See More" : "See Less"}</button>
                     </div>
                     {/*FOURTH CARD*/} 
                     <div className={card}> 
@@ -74,9 +92,17 @@ const AboutPage = () => (
                         </div>
                         <h3 className={personsName}>Julio Sueiras</h3>
                         <h4 className={title}>DevOps, Founder</h4>
-                        <p className={paragraph}>
-                            Julio loves automation. He's helped companies streamline their IT infrastructure. Fun fact: Julio was born in China, raised in Cuba and speaks 3 languages (Mandarin, Spanish and English).
-                        </p>
+
+                        <div className={!cardsState.julio ? cardDetailsContainerClosed : cardDetailsContainerOpen}>
+                          <p className={paragraph}>
+                              Quite simply, Julio loves automation and has helped numerous companies streamline their IT infrastructure.  
+                          </p>
+                          <p className={paragraph}>As a DevOps Engineer, his passion is automating infrastructure, however he consistently demonstrates his willingness to pitch in anywhere necessary to ensure other team members can remained focussed on helping students and employers.  </p>
+                          <p className={paragraph}>To say that Julio is multi-cultural is an understatement.  Julio was born in Cuba, raised in China and speaks 3 languages fluently (Mandarin, Spanish and English).  He is a phenomenon in more ways than one to be sure!
+                          </p>
+                        </div>
+
+                        <button className={seeMoreButton} onClick={() => toggleCardOpen('julio')}>{!cardsState.julio ? "See More" : "See Less"}</button>
                     </div>
                 </div>
             </div>
